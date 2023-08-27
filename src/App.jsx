a5521e3992ecdba1ab05fd09eb426fffe4c4e5b9
Aryan Patel <patelaryan7751@gmail.com>
2023-08-27 08:51:14 +0530

    added typescript to the project

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-function App() {
-  return (
-    <>
-    <h1 className="text-xl text-blue-500">Hello World!</h1>
-     </>
-  )
-}
-export default App
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl text-blue-500">Hello World!</h1>
+    </>
+  );
+}
+export default App;
