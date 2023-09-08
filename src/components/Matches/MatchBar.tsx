8dd601129ce0c7432abbadaaadebe78377e88aba
Aryan Patel <patelaryan7751@gmail.com>
2023-09-08 23:54:30 +0530

    get match results from api endpoint in console

--- src/components/Matches/MatchBar.tsx
+++ src/components/Matches/MatchBar.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { API_ENDPOINT } from "../../config/constants";
 
-function MatchBar() {
+const MatchBar = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${API_ENDPOINT}/matches`);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log(data);
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <>
       <div className="flex-shrink-0 w-80 bg-white shadow-lg mx-2 p-4 rounded-md">
@@ -10,7 +31,22 @@ function MatchBar() {
             <p className="text-sm font-normal">IPL 2023,Delhi</p>
           </div>
           <div>
-            <p>Refresh</p>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 "
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                />
+              </svg>
+            </button>
           </div>
         </div>
         <div className="flex justify-between mt-2">
@@ -32,6 +68,6 @@ function MatchBar() {
       </div>
     </>
   );
-}
+};
 
 export default MatchBar;
